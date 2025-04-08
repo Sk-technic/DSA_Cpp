721a564edfb6b507c74ec99956d634d017f2d3b5
Sk-technic <ssk113054@gmail.com>
2025-04-08 13:56:43 +0530

    eighth pattern

---
+++ patterns_in_c++/pattern_8.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// 1
+// 23
+// 345
+// 4567
+
+
+int main(){
+    int i = 1;
+    int n;
+    cout <<"enter the number";
+    cin>> n;
+
+    while (i<=n)
+    {
+        int j = 1;
+        int count = i;
+        while (j<=i)
+        {
+            cout<< count;
+            j++;
+            count++;
+        }
+        cout<< endl;
+        i++;
+    }
+    
+}
